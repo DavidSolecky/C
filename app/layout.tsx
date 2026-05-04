e7bb3e31bf0eaebe7a0829253737c0b43395aa10
DavidSolecky <davidsolecky@gmail.com>
2026-05-04 11:44:28 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ export const metadata = {
   description: "Sledovanie výroby",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sk">
       <body>{children}</body>
