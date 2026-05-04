7e6ed935de1a991721d304b537fb5fd99fd3995e
DavidSolecky <davidsolecky@gmail.com>
2026-05-04 11:30:00 +0200

    app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>SOF-Track je spustený 🚀</h1>
+      <p>Systém sledovania výroby funguje.</p>
+    </div>
+  );
+}
