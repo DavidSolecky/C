41c54944ab6ae63274299506b4f88d1d8857e568
DavidSolecky <davidsolecky@gmail.com>
2026-05-04 11:38:40 +0200

    app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "SOF-Track",
+  description: "Sledovanie výroby",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sk">
+      <body>{children}</body>
+    </html>
+  );
+}
